702fb06f9aeb28769a057608b67afa34a2972319
ShawnATX <volushawn@gmail.com>
2020-04-22 18:26:58 -0500

    Updates

---
+++ precache-manifest.7d8e507c6472aa51590f4080fb793f67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f23c0cd550649234c54b857c9f9fd8a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "c2259562d5d6b07dd153",
+    "url": "/React-Portfolio/static/css/main.28825860.chunk.css"
+  },
+  {
+    "revision": "041ae1bd64cefaf00485",
+    "url": "/React-Portfolio/static/js/2.a5aa96bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.a5aa96bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2259562d5d6b07dd153",
+    "url": "/React-Portfolio/static/js/main.19dd7872.chunk.js"
+  },
+  {
+    "revision": "377be9033b1ae50e0df6",
+    "url": "/React-Portfolio/static/js/runtime-main.3754d297.js"
+  },
+  {
+    "revision": "34b80bcde64c5f374bb8d57e80bb0c0d",
+    "url": "/React-Portfolio/static/media/workbench.34b80bcd.jpg"
+  }
+]);
